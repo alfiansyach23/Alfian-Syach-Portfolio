23ffef2ebfe97077ccd97856c9619730707aad32
Alfian Syach <127624933+alfiansyach23@users.noreply.github.com>
2026-02-07 21:07:03 +0700

    Update vite.config.js

--- Alfian-Syach-Portfolio/vite.config.js
+++ Alfian-Syach-Portfolio/vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      // Menggunakan path.resolve dengan nama folder langsung
+      "@": path.resolve(process.cwd(), "./src"),
     },
-    // Tambahkan baris di bawah ini:
     extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json']
   },
 })
