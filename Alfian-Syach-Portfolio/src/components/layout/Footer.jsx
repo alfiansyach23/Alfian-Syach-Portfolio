71b6a1e513a519dfa231e84e1e47e627ca524e4c
Alfian Syach <127624933+alfiansyach23@users.noreply.github.com>
2026-02-08 00:31:06 +0700

    Update Footer.jsx
    Delete logo

--- Alfian-Syach-Portfolio/src/components/layout/Footer.jsx
+++ Alfian-Syach-Portfolio/src/components/layout/Footer.jsx
@@ -1,17 +1,11 @@
-import React from 'react'
-
 export default function Footer() {
   return (
-    <div className='bg-black px-5 lg:px-28 py-3 lg:py-6 flex items-center justify-between mt-16'>
-      <img className='invert h-5 lg:h-9' src="assets/images/logo2.svg" alt="" />
-
+    <div className='bg-black px-5 lg:px-28 py-3 lg:py-6 flex items-center justify-end mt-16'>
+      {/* Gambar dihapus, tinggal tambahkan justify-end jika ingin teks tetap di kanan */}
       <div className='text-white lg:font-semibold lg:text-sm font-normal text-[10px] text-right lg:space-y-3'>
         <p>@ 2025 Personal Portfolio</p>
-        <p>Made by -SK</p>
+        <p>Made by -ALFIN</p>
       </div>
     </div>
   )
 }
-
-
-
